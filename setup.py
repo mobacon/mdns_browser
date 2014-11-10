71fbe827807c56a310225e61220bb3eb95340f28
Sven Schlender <none@none>
2014-11-10 16:54:57 +0100

    Fixed this for using with setup.py.

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ if sys.version_info[0] != 2 and sys.version_info[1] != 7:
     exit()
 
 setup(name='mdns_browser',
-      version='1.0',
-      description='GUI for MDNS discovering of HTTP services in a LAN.',
-      author='Sven Schlender',
-      author_email='kontakt@mobacon.de',
-      url='http://www.mobacon.de',
-      packages=find_packages(),
-      entry_points={
-          'console_scripts': [
-               'snappy_generator = mdns_browser:main']},
-      install_requires = ['zeroconf'],
-     )
+    version='1.0',
+    description='GUI for MDNS discovering of HTTP services in a LAN.',
+    author='Sven Schlender',
+    author_email='kontakt@mobacon.de',
+    url='http://www.mobacon.de',
+    packages=find_packages(),   
+    entry_points={
+      'gui_scripts': [
+           'mdns_browser = src.mdns_browser:main']},
+    install_requires = ['zeroconf'],
+)
